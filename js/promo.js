f9efe1e468c9b08fd90f4e973a84b4f1cc4e8b64
alejocalderon90 <santiago.alejo.calderon@gmail.com>
2023-11-11 14:08:09 -0300

    se crea pagina corporativo y sedes

--- js/promo.js
+++ js/promo.js
@@ -7,7 +7,7 @@ function cargarPromo (){
     div.innerHTML = `
     <img src="../img/promo-popup.webp">
     <button class="btn btn-danger top" onclick="cerrarPromo()">X</button>
-    <a class="banner__items__boton position" href="https://api.whatsapp.com/send/?phone=5492304513220&text&type=phone_number&app_absent=0" target="_blank"><button>QUIERO LA PROMO</button></a>
+    <a class="banner__items__boton position" href="https://api.whatsapp.com/send/?phone=5492304513220&text=Hola! Quiero un sitio web para mi emprendimiento." target="_blank"><button>QUIERO LA PROMO</button></a>
     `
     promo.appendChild(div)
 }
