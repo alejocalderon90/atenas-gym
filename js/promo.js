0f04b7d1191c24365b69177887b0337062d051a0
alejocalderon90 <santiago.alejo.calderon@gmail.com>
2023-11-14 14:25:22 -0300

    se finalizan las paginas

--- js/promo.js
+++ js/promo.js
@@ -5,7 +5,7 @@ function cargarPromo (){
     const div = document.createElement("div")
     div.classList.add("divPromo")
     div.innerHTML = `
-    <img src="../img/promo-popup.webp">
+    <img src="./img/promo-popup.webp">
     <button class="btn btn-danger top" onclick="cerrarPromo()">X</button>
     <a class="banner__items__boton position" href="https://api.whatsapp.com/send/?phone=5492304513220&text=Hola! Quiero un sitio web para mi emprendimiento." target="_blank"><button>QUIERO LA PROMO</button></a>
     `
